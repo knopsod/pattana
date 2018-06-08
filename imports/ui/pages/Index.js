8f2fb2e40676fb5df5f67c83a65e580f6d7d958f
Noppadon Sodram <knopsod@gmail.com>
2018-06-08 11:45:32 +0700

    Edit discription

--- imports/ui/pages/Index.js
+++ imports/ui/pages/Index.js
@@ -5,12 +5,12 @@ const Index = () => (
   <div className="Index">
     <Jumbotron className="text-center">
       <h2>โปรแกรมกองทุนหมู่บ้านพัฒนา</h2>
-      <p>โปรแกรมออนไลน์ เข้าถึงง่าย ใช้สะดวก ลดความผิดพลาด ประหยัดเวลา เพิ่มประสิทธิภาพการบริหาร</p>
-      <p>โปรแกรมนี้พัฒนาจากโครงการ Open Source ไลเซนส์ MIT ท่านสามารถคัดลอกและนำไปใช้พัฒนาได้ภายใต้ไลเซนส์เดียวกัน</p>
+      <p>เข้าถึงง่าย ใช้สะดวก ลดความผิดพลาด ประหยัดเวลา เพิ่มประสิทธิภาพการบริหาร</p>
+      <p>พัฒนาจาก Open Source ไลเซนส์ MIT ท่านสามารถคัดลอกและนำไปใช้ได้ภายใต้ไลเซนส์นี้</p>
       <p><a className="btn btn-success" href="https://github.com/themeteorchef/base"
-      role="button">themeteorchef/base</a></p>
+      role="button">GitHub: themeteorchef/base</a></p>
       <p><a className="btn btn-success" href="https://github.com/knopsod/pattana"
-      role="button">knopsod/pattana</a></p>
+      role="button">GitHub: knopsod/pattana</a></p>
       <p style={ { fontSize: '16px', color: '#aaa' } }>Currently at v4.15.0</p>
     </Jumbotron>
   </div>
