4fbe1c50580982be13f2eee506feb11072e7f050
Noppadon Sodram <knopsod@gmail.com>
2018-06-08 11:25:35 +0700

    Edit Navbar.Brand message

--- imports/ui/components/AppNavigation.js
+++ imports/ui/components/AppNavigation.js
@@ -13,7 +13,7 @@ const AppNavigation = ({ hasUser }) => (
   <Navbar>
     <Navbar.Header>
       <Navbar.Brand>
-        <Link to="/">Application Name</Link>
+        <Link to="/">โปรแกรมกองทุนหมู่บ้านพัฒนา</Link>
       </Navbar.Brand>
       <Navbar.Toggle />
     </Navbar.Header>
