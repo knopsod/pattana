8eb2bbb7c236ff14dded96968e0e089e18e8e67c
Noppadon Sodram <knopsod@gmail.com>
2018-06-08 11:38:08 +0700

    Add discripttion at index page

--- imports/ui/pages/Index.js
+++ imports/ui/pages/Index.js
@@ -4,9 +4,13 @@ import { Jumbotron } from 'react-bootstrap';
 const Index = () => (
   <div className="Index">
     <Jumbotron className="text-center">
-      <h2>Base</h2>
-      <p>A starting point for Meteor applications.</p>
-      <p><a className="btn btn-success" href="https://themeteorchef.com/base" role="button">Read the Documentation</a></p>
+      <h2>โปรแกรมกองทุนหมู่บ้านพัฒนา</h2>
+      <p>โปรแกรมออนไลน์ เข้าถึงง่าย ใช้สะดวก ลดความผิดพลาด ประหยัดเวลา เพิ่มประสิทธิภาพการบริหาร</p>
+      <p>โปรแกรมนี้พัฒนาจากโครงการ Open Source ไลเซนส์ MIT ท่านสามารถคัดลอกและนำไปใช้พัฒนาได้ภายใต้ไลเซนส์เดียวกัน</p>
+      <p><a className="btn btn-success" href="https://github.com/themeteorchef/base"
+      role="button">themeteorchef/base</a></p>
+      <p><a className="btn btn-success" href="https://github.com/knopsod/pattana"
+      role="button">knopsod/pattana</a></p>
       <p style={ { fontSize: '16px', color: '#aaa' } }>Currently at v4.15.0</p>
     </Jumbotron>
   </div>
